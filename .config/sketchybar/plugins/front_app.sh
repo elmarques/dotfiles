6b6666a03b3e45b2aa464cd3717a1a076ec0118e
El Marques <el@dachs.tech>
2024-05-24 17:08:22 +0100

    (neovim) improve SketchyBar design

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+source "$CONFIG_DIR/plugins/icon_map.sh"
+
+__icon_map "$INFO"
+
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  sketchybar --set "$NAME" label="$INFO" icon="$icon_result"
 fi
